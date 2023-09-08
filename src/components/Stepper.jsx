4d640a2ea2548c8ef1ccc9a8a7aca206b655de68
Anirudh <anirudhece2020@gmail.com>
2023-09-09 02:01:50 +0530

    stepper done

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -6,19 +6,15 @@ import StepLabel from "@mui/material/StepLabel";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-const steps = [
-  "user will signup/login",
-  "user will create profile",
-  "symptom add",
-  "appointment is booked ",
-];
+// add the name of each step
+const steps = ["signup/login", "profile", "concern", "appointment"];
 
 export default function HorizontalStepper() {
   const [activeStep, setActiveStep] = React.useState(0);
   const [skipped, setSkipped] = React.useState(new Set());
 
   const isStepOptional = () => {
-    return activeStep === 1; // Only step 1 is optional
+    return activeStep === 1; //add which step you want to make optional
   };
 
   const isStepSkipped = (step) => {
@@ -57,6 +53,25 @@ export default function HorizontalStepper() {
     setActiveStep(0);
   };
 
+  const renderText = (text) => {
+    return (
+      <Box mt={6} mb={6} sx={{ display: "flex", justifyContent: "center" }}>
+        <Typography
+          variant="h4"
+          sx={{
+            textAlign: "center",
+            fontWeight: "800",
+            color: "rgb(32, 40, 45)",
+            fontFamily: '"Nunito Sans", sans-serif',
+            width: "90%",
+          }}
+        >
+          {text}
+        </Typography>
+      </Box>
+    );
+  };
+
   const renderButtons = () => {
     return (
       <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
@@ -93,6 +108,7 @@ export default function HorizontalStepper() {
           const stepProps = {};
           const labelProps = {};
           if (isStepOptional() && index === 1) {
+            // also edit this line of optional index is changed
             labelProps.optional = (
               <Typography variant="caption">Optional</Typography>
             );
@@ -111,36 +127,44 @@ export default function HorizontalStepper() {
       {activeStep === 0 ? (
         <>
           <div>
-            test0
+            {renderText(
+              `The user will land and proceed to 🔑 log in or ✍️ sign up`
+            )}
             {renderButtons()}
           </div>
         </>
       ) : activeStep === 1 ? (
         <>
           <div>
-            test1
+            {renderText(
+              `The user will create their profile by adding details such as their name, age, 📋 previous medical records, or gender`
+            )}
             {renderButtons()}
           </div>
         </>
       ) : activeStep === 2 ? (
         <>
           <div>
-            test2
+            {renderText(
+              `The user can provide details about their symptoms, how long they have been feeling unwell, and any pre-existing conditions. 💊`
+            )}
             {renderButtons()}
           </div>
         </>
       ) : activeStep === 3 ? (
         <>
           <div>
-            test3
+            {renderText(
+              `Voilà! 📅 Appointment is booked, and confirmation details will be dispatched from our end to user`
+            )}
             {renderButtons()}
           </div>
         </>
       ) : activeStep === steps.length ? (
         <>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you're finished
-          </Typography>
+          {renderText(
+            `The details of the patient with potential disease, figured out by AI, ML, and Deep Learning🤖, will be relayed to the concerned medical 👩‍⚕️staff at the targeted hospital 🏥.  `
+          )}
           {renderButtons()}
         </>
       ) : null}
