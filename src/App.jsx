97adf3fdeb468b7f09e1ae59eaaa0ba29ab5f884
Anirudh <anirudhece2020@gmail.com>
2023-09-10 15:30:20 +0530

    contact and stats added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Stepper from "./components/Stepper";
 import { Box } from "@mui/material";
 import Hero from "./components/Hero";
 import Navigation from "./components/Navigation";
+import { ChakraProvider } from "@chakra-ui/react";
+import Stats from './components/Stats'
+import Contact from "./components/Contact";
+
 function App() {
   return (
     <>
@@ -15,9 +19,13 @@ function App() {
           mt: 25,
         }}
       >
-        <Box sx={{ width: "85%" }}>
-          <Hero />
-          <Stepper />
+        <Box sx={{ }}>
+          {/* <Hero />
+          <Stepper /> */}
+          <ChakraProvider>
+            {/* <Contact /> */}
+            <Stats/>
+          </ChakraProvider>
         </Box>
       </Box>
     </>
